29ef9c165f9428f8368dd78585c2868c9a4354cb
Pablo Facciano <pabloezequielfacha@gmail.com>
2021-12-14 19:36:38 -0300

    3rd try

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser')
 const submodules = [
     require('./modules/discount-code/app/app'),
     require('./modules/discount-redemption-split/app/app'),
-    require('./modules/discount-code-copy/app/app'),
+    require('./modules/custom-activity/app/app'),
 ];
 
 const app = express();
