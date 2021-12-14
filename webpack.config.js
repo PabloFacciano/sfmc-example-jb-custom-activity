5f23882e68aef322d5c519bed64f2ec4977fb3f6
Pablo Facciano <pabloezequielfacha@gmail.com>
2021-12-14 19:40:10 -0300

    4th try

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
 const discountCodeExample = require('./modules/discount-code/webpack.config');
 const splitExample = require('./modules/discount-redemption-split/webpack.config');
+const customActivity = require('./modules/custom-activity/webpack.config');
 
 module.exports = function(env, argv) {
     return [
         discountCodeExample(env, argv),
         splitExample(env, argv),
+        customActivity(env, argv),
     ];
 };
